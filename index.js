14d8e5bd149d802d6797107877889b23b83999df
JasonMcGee0715 <jdm0715@gmail.com>
2020-07-13 21:18:55 -0500

    starting today

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 const { users } = require('./state')
 
+//Basic info you need for it to show in browser.
+// app.get('/', (req, res) => res.send('Hello World'))
+
+
 /* BEGIN - create routes here */
+// app.get('/users', (req, res) => {
+// res.json(users)
+// })
+
+app.get('/users/1', (req, res) => {
+  return res.json(users[0]);
+  })
+
+app.get('/users/:userId', (req, res) => {
+  const id = req.params.userId
+  console.log(id)
+
+  const filteredUsers = users.filter((user) => user._id === Number(id))
+  console.log(filteredUsers)
+  })
 
 
 /* END - create routes here */
