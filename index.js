5a5cfbfe84154f44c4769226b278dece8c71f277
JasonMcGee0715 <jdm0715@gmail.com>
2020-07-16 10:37:04 -0500

    finished up to basic post

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-const { users } = require('./state')
+const { users } = require('./state');
+const bodyParser = require('body-parser');
+
+const counter = users.length;
 
 //Basic info you need for it to show in browser.
 // app.get('/', (req, res) => res.send('Hello World'))
 
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 /* BEGIN - create routes here */
-// app.get('/users', (req, res) => {
-// res.json(users)
-// })
+app.get('/users', (req, res) => {
+res.json(users)
+})
 
 app.get('/users/1', (req, res) => {
   return res.json(users[0]);
@@ -20,11 +27,30 @@ app.get('/users/1', (req, res) => {
 
 app.get('/users/:userId', (req, res) => {
   const id = req.params.userId
+  console.log(req)
   console.log(id)
-
   const filteredUsers = users.filter((user) => user._id === Number(id))
-  console.log(filteredUsers)
+  res.json(filteredUsers)
   })
+
+app.post('/users', (req, res) => {
+  users.push({
+    _id: (counter + 1),
+    ...req.body
+  });
+  // const newUser = {
+  //   _id: (counter += 1),
+  //   Name: req.body.name,
+  //   Occupation: req.body.occupation,
+  //   Avatar: req.body.avatar
+  // }
+  // users.push(newUser);
+  res.json(users);
+})
+
+app.put('users/1', (req, res) => {
+  const found = users.some(user => user.id === Number(req.params.id)
+})
 
 
 /* END - create routes here */
