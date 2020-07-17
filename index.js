73d1680ec0962ed9b600a039ded81f425208d2f7
JasonMcGee0715 <jdm0715@gmail.com>
2020-07-16 21:40:54 -0500

    working on Dynamic PUT / Read.me Part 3

--- index.js
+++ index.js
@@ -17,22 +17,18 @@ app.use(express.urlencoded({extended: false}));
 
 
 /* BEGIN - create routes here */
+//Basic GET
 app.get('/users', (req, res) => {
 res.json(users)
 })
 
+//Basic Get User 1
 app.get('/users/1', (req, res) => {
   return res.json(users[0]);
   })
 
-app.get('/users/:userId', (req, res) => {
-  const id = req.params.userId
-  console.log(req)
-  console.log(id)
-  const filteredUsers = users.filter((user) => user._id === Number(id))
-  res.json(filteredUsers)
-  })
 
+  //Basic POST and Part-2 POST
 app.post('/users', (req, res) => {
   users.push({
     _id: (counter + 1),
@@ -48,13 +44,41 @@ app.post('/users', (req, res) => {
   res.json(users);
 })
 
+//Basic PUT
+app.put('/users/1', (req, res) => {
+  users.forEach(user => {
+    if (user._id == 1) {
+      user.name = req.body.name;
+      user.occupation = req.body.occupation;
+      user.avatar = req.body.avatar;
+    }
+    res.json(users)
+  })
+})
+
+//Basic DELETE
+app.delete('/users/1', (req, res) => {
+  let newUsers = users.slice(1);
+  res.json(newUsers)
+})
+
+//Dynamic GET
+app.get('/users/:userId', (req, res) => {
+  const id = req.params.userId
+  console.log(req)
+  console.log(id)
+  const filteredUsers = users.filter((user) => user._id === Number(id))
+  res.json(filteredUsers)
+  })
+
+//Dynamic PUT
 app.put('users/:id', (req, res) => {
-  const found = users.some(user => user.id === Number(req.params.id));
+  const found = users.some(user => user._id === Number(req.params.id));
 
   if (found) {
     const updatedUser = req.body;
     users.forEach(user => {
-      if(user.id === Number(req.params.id)) {
+      if(user._id === Number(req.params.id)) {
         user.name = updatedUser.name ? updatedUser.name : user.name;
         user.occupation = updatedUser.occupation ? updatedUser.occupation : user.occupation;
         user.avatar = updatedUser.avatar ? updatedUser.avatar : user.avatar;
@@ -68,6 +92,11 @@ app.put('users/:id', (req, res) => {
   res.json(users)
   }
 )
+
+
+
+
+
 
 
 /* END - create routes here */
